9126ed8748986dda1ed5ad2155c289e1aed81cec
leechanwoo-kor <cwlee1124@gmail.com>
2025-08-18 11:17:32 +0900

    :zap: Update MusicControlView timeout to 2 hours for extended button availability

--- tapi/modules/music_views.py
+++ tapi/modules/music_views.py
@@ -228,7 +228,7 @@ class RecommendationView(discord.ui.View):
 
 class MusicControlView(discord.ui.View):
     def __init__(self, cog, guild_id):
-        super().__init__(timeout=1800)  # 30분 후 버튼 비활성화
+        super().__init__(timeout=7200)  # 2시간 후 버튼 비활성화
         self.cog = cog
         self.guild_id = guild_id
 
