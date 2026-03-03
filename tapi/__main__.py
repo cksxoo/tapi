4a36f8c4ea3072c45e74d5f95f15a3ecd5f51039
leechanwoo-kor <cwlee1124@gmail.com>
2026-03-03 16:22:18 +0900

    fix: update interaction command check to allow channel settings changes

--- tapi/__main__.py
+++ tapi/__main__.py
@@ -81,8 +81,8 @@ class TapiBot(commands.Bot):
         if interaction.guild is None:
             return True
 
-        # /channel 명령어 자체는 채널 제한 없이 허용
-        if interaction.command and interaction.command.name == "channel":
+        # /setting 명령어는 채널 제한 없이 허용 (채널 설정 변경을 위해 필요)
+        if interaction.command and interaction.command.name == "setting":
             return True
 
         # DB에서 봇 전용 채널 조회
